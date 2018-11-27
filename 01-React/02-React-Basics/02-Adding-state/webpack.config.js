20350b300aafe3935c356ea3fb481e4e09b493e7
Edward Schults <edward.schults@gmail.com>
2018-11-27 12:24:01 +0100

    upgrade packages in 02-Adding-state

--- 01-React/02-React-Basics/02-Adding-state/webpack.config.js
+++ 01-React/02-React-Basics/02-Adding-state/webpack.config.js
@@ -2,15 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "./lib/01_article_with_state.jsx"),
+  mode: "development",
   output: {
-    filename: "dist/bundle.js"
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
   },
+  devtool: "sourcemap",
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  },
-  devtool: "sourcemap"
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
 };
