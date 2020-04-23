8d36ec5453c567fa426ea65930a4f26575640596
yvan poussard <yvs.pro64@gmail.com>
2020-04-23 15:24:08 +0200

    ok with soluce

--- 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
+++ 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
@@ -1,10 +1,18 @@
 class Counter {
   constructor(text) {
     // TODO: build an internal Map of word => occurrences.
+    this.map = new Map();
+    if (text && text.length > 0) {
+      text.split(" ").forEach((item) => {
+        const upper = item.toUpperCase();
+        this.map.set(upper, (this.map.get(upper) || 0) + 1);
+      });
+    }
   }
 
   occurrences(word) {
     // TODO: return the number of occurrences
+    return this.map.get(word.toUpperCase()) || 0;
   }
 }
 
