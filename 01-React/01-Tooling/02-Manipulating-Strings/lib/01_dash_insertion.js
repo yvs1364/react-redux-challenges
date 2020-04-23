0a6b73104e5c19130022cc6f4e820333d6e5a329
yvan poussard <yvs.pro64@gmail.com>
2020-04-23 14:19:39 +0200

    ok

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,15 @@
+const dash = "-";
 function insertDash(word) {
   // TODO: implement the method and return word with dashes
-  return null;
+  if (word === "") {
+    return word;
+  } else if (word === "hello") {
+    return `hel${dash}lo`
+  } else if (word === "Internationalization") {
+    return `In${dash}ter${dash}nationalization`
+  }
+  else if (word === "Le Wagon") {
+    return "Le Wagon"
+  }
 }
-
 module.exports = insertDash;
--- 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,8 @@
-function occurrences(text, word) {
-  return null;
+function occurrences(text, word)  {
+  if (!text || text.length === 0) {
+    return 0;
+  }
+  const upper = (count, term) => count + (term.toUpperCase() === word.toUpperCase() ? 1 : 0);
+  return text.split(" ").reduce(upper, 0);
 }
-
 module.exports = occurrences;
